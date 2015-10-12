8aefc239100233b80b5f15fe8ef068b49178f0bf
martinsimonetti <msimonetti@gmail.com>
2015-10-12 10:56:01 -0300

    Primer Commit. Se agrega el proyecto completo.

---
+++ js/controlGrilla.js
@@ -0,0 +1,13 @@
+
+     
+		function Borrar(id)
+		{
+			document.getElementById('idParaBorrar').value = id;
+			document.frmBorrar.submit();
+		}
+		function Modificar(id)
+		{
+			document.getElementById('idParaModificar').value = id;
+			document.frmModificar.submit();
+		}
+   
